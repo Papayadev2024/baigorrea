46fc7c8d0f23940259e896e77f676b12278ab2c4
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-03-26 16:18:32 -0500

    final

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -45,6 +45,7 @@ class DatabaseSeeder extends Seeder
             NosotrosViewSeeder::class,
             ContactoViewSeeder::class,
             ImagenProductoSeeder::class,
+            NewUserSeeder::class,
 
             //ServiceSeeder::class,
         ]);
