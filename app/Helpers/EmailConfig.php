3b2a289a6077de819418df4112e227947351cf8a
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-04-26 10:52:52 -0500

    final

--- app/Helpers/EmailConfig.php
+++ app/Helpers/EmailConfig.php
@@ -11,15 +11,15 @@ class EmailConfig
         $mail = new PHPMailer(true);
         // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
         $mail->isSMTP();
-        $mail->Host = 'mail.draadrianapezo.com';
+        $mail->Host = 'mail.baigorreagroup.com';
         $mail->SMTPAuth = true;
-        $mail->Username = 'info@draadrianapezo.com';
-        $mail->Password = 'adrianapezo2025#';
+        $mail->Username = 'info@baigorreagroup.com';
+        $mail->Password = 'info2025#';
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port = 465;
         $mail->Subject = '' . $name . ', ' . $mensaje;
         $mail->CharSet = 'UTF-8';
-        $mail->setFrom('info@draadrianapezo.com', 'Adriana Pezo');
+        $mail->setFrom('info@baigorreagroup.com', 'Baigorrea Group');
         return $mail;
     }
 }
